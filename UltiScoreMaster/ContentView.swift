c91b70f749e13e8d2ad1531b0f6cb10c38373671
haruki-mochida <motimotimotida@gmail.com>
2024-01-19 23:56:54 +0900

    Initial Commit

---
+++ UltiScoreMaster/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  UltiScoreMaster
+//
+//  Created by 持田晴生 on 2024/01/19.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ UltiScoreMaster/UltiScoreMasterApp.swift
@@ -0,0 +1,17 @@
+//
+//  UltiScoreMasterApp.swift
+//  UltiScoreMaster
+//
+//  Created by 持田晴生 on 2024/01/19.
+//
+
+import SwiftUI
+
+@main
+struct UltiScoreMasterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
